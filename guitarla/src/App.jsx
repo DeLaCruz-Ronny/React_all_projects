38b27e35ed688854a5dad191ff41ec16ca8d9d85
Ronny Manuel De la Cruz Perez <62533639+DeLaCruz-Ronny@users.noreply.github.com>
2024-06-20 16:41:25 -0400

    Agregando cantidades al carrito

--- guitarla/src/App.jsx
+++ guitarla/src/App.jsx
@@ -5,6 +5,20 @@ import { db } from "./data/db";
 
 function App() {
   const [data, setData] = useState(db);
+  const [cart, setCart] = useState([])
+
+  const addToCart = (item) =>{
+    const itemExists = cart.findIndex(guitar => guitar.id === item.id)
+    if (itemExists >= 0) {
+      const updateCart = [...cart]
+      updateCart[itemExists].quantity++
+      setCart(updateCart)
+    }else{
+      item.quantity = 1
+      setCart(prevCart => [...prevCart, item])
+    }
+
+  }
 
   return (
     <>
@@ -13,9 +27,13 @@ function App() {
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
-          {data.map((guitar) => {
-            return <Guitar key={guitar.id} guitar={guitar} />;
-          })}
+          {data.map((guitar) => (
+            <Guitar 
+              key={guitar.id} 
+              guitar={guitar} 
+              addToCart={addToCart}
+            />
+          ))}
         </div>
       </main>
 
